52a6693ad4ff0a5043184e1bd85607cea8d18efb
Mochammad Fachrizal Muzakky <zakky092@gmail.com>
2024-03-20 07:30:27 +0700

    merubah link sidebar brand

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,7 +24,7 @@
     <!-- Sidebar -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
         <div class="sidebar-brand-icon">
           <i class="fas fa-fingerprint"></i>
         </div>
