8bbef4f6ced6419a9acd60cec8cd4569ab3e6c8f
Mochammad Fachrizal Muzakky <zakky092@gmail.com>
2024-03-21 10:57:14 +0700

    menambahkan rute untuk cetak-pdf

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index']);
+Route::get('/cetak-pdf', [MainController::class, 'cetak_pdf']);
