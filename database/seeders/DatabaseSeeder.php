73bdd84cfc03dd6211bacc2f978299bc73b55547
Mochammad Fachrizal Muzakky <zakky092@gmail.com>
2024-04-02 09:23:00 +0700

    Add seeder for user table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,37 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::create([
+      'name' => 'Mustofa',
+      'email' => 'mustofa@gmail.com',
+      'password' => bcrypt('password'),
+    ]);
+    User::create([
+      'name' => 'Ulum',
+      'email' => 'ulum@gmail.com',
+      'password' => bcrypt('password'),
+    ]);
+    User::create([
+      'name' => 'Roni',
+      'email' => 'roni@gmail.com',
+      'password' => bcrypt('password'),
+    ]);
+    User::create([
+      'name' => 'Supri',
+      'email' => 'supri@gmail.com',
+      'password' => bcrypt('password'),
+    ]);
+    User::create([
+      'name' => 'Eko',
+      'email' => 'eko@gmail.com',
+      'password' => bcrypt('password'),
+    ]);
+  }
 }
