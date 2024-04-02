e7d0971d081e6da076bb2ee54877d198734690f5
Mochammad Fachrizal Muzakky <zakky092@gmail.com>
2024-04-02 10:25:51 +0700

    Get data from database for dynamic users.index views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController
@@ -11,7 +12,10 @@ class UserController
    */
   public function index()
   {
-    return view('users.index');
+    return view('users.index', [
+      "employees" => User::all(),
+      "title" => "Daftar Karyawan",
+    ]);
   }
 
   /**
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,38 +3,38 @@
 @section('content')
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-  <h1 class="h3 mb-0 text-gray-800">Cards</h1>
+  <h1 class="h3 mb-0 text-gray-800">Daftar Karyawan</h1>
 </div>
 
 <div class="row">
-
-  <div class="col-lg-4">
+  @foreach ($employees as $employee)
+  <div class="col-lg-3">
     <!-- Dropdown Card Example -->
     <div class="card shadow mb-4">
       <!-- Card Header - Dropdown -->
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">Dropdown Card Example</h6>
+        <h6 class="m-0 font-weight-bold text-primary">{{ $employee->name }}</h6>
         <div class="dropdown no-arrow">
           <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-            <div class="dropdown-header">Dropdown Header:</div>
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-header">Menu:</div>
+            <a class="dropdown-item" href="#">Lihat Detail</a>
+            <a class="dropdown-item" href="#">Edit Data</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="#">Hapus Data</a>
           </div>
         </div>
       </div>
       <!-- Card Body -->
       <div class="card-body">
-        Dropdown menus can be placed in the card header in order to extend the functionality
-        of a basic card. In this dropdown card example, the Font Awesome vertical ellipsis
-        icon in the card header can be clicked on in order to toggle a dropdown menu.
+        <div class="text-center">
+          <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="img/undraw_profile_2.svg" alt="...">
+        </div>
       </div>
     </div>
   </div>
-
+  @endforeach
 </div>
 @endsection
