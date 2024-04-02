73a26f9b35ef4079c4edc739d03d0e84ca48fff5
Mochammad Fachrizal Muzakky <zakky092@gmail.com>
2024-04-02 10:04:48 +0700

    Add UserController & users.index view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -75,7 +75,7 @@
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Manajemen Karyawan:</h6>
-            <a class="collapse-item" href="utilities-color.html">Daftar Karyawan</a>
+            <a class="collapse-item" href="/users">Daftar Karyawan</a>
             <a class="collapse-item" href="utilities-border.html">Tambah Karyawan</a>
             <a class="collapse-item" href="utilities-animation.html">Edit Karyawan</a>
             <a class="collapse-item" href="utilities-other.html">Hapus Karyawan</a>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [MainController::class, 'index']);
+Route::controller(UserController::class)->group(function () {
+  Route::resource('/users', UserController::class);
+});
